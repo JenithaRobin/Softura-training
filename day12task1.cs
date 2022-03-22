6aceac4ec59d9645929ff282e9c081fe74010958
JenithaRobin <101104724+JenithaRobin@users.noreply.github.com>
2022-03-22 16:10:02 +0530

    Add files via upload

---
+++ day12task1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class day12task1
+    {
+        public static void Main()
+        {
+
+            FileStream fs = new FileStream(@"D:\csharp\Desert.txt",FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter bw = new StreamWriter(fs);
+            List<string> str = new List<String>();
+            str.Add("Gulab Jamun");
+            str.Add("Halwa");
+            str.Add("kaju katli");
+            str.Add("laddo");
+            str.Add("paal Kova");
+            foreach (string d in str)
+            {
+             bw.Write("\n" + d);
+            }
+            bw.Flush();
+            bw.Close();
+            fs.Close();
+            FileStream fs1 = new FileStream(@"D:\\csharp\\Dessert.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamReader sr = new StreamReader(fs1);
+            String s = sr.ReadToEnd();
+            sr.Close();
+            fs1.Close();
+            Console.WriteLine(s);
+            FileInfo fi = new FileInfo(@"D:\csharp\Dessert.txt");
+            Console.WriteLine(fi.Length);
+            Console.WriteLine(fi.CreationTime);
+            Console.Read();
+        }
+    }
+}
---
+++ day12task2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace ConsoleApp1
+
+
+{
+    [Serializable]
+    class empdetails
+    {
+
+        public string name = "Jenitha";
+        public int age = 20;
+        public string Empid = "456123E";
+        public string gender = "Female";
+
+    }
+    class sereialization
+    {
+        public void SeretoFile()
+        {
+            empdetails sobj = new empdetails();
+            FileStream fs = new FileStream(@"D:\csharp\empdetails.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            BinaryFormatter f = new BinaryFormatter();
+            f.Serialize(fs, sobj);
+            fs.Close();
+        }
+        public void Dserial()
+        {
+            FileStream fs = new FileStream(@"D:\csharp\empdetails.txt", FileMode.Open, FileAccess.Read);
+            BinaryFormatter f = new BinaryFormatter();
+            empdetails stu = (empdetails)f.Deserialize(fs);
+            Console.WriteLine(stu.name);
+            Console.WriteLine(stu.age);
+            Console.WriteLine(stu.gender);
+            Console.WriteLine(stu.Empid);
+
+
+
+        }
+
+        public static void Main()
+        {
+            sereialization lobj = new sereialization();
+            lobj.SeretoFile();
+            lobj.Dserial();
+            Console.ReadLine();
+
+
+        }
+    }
+
+}
